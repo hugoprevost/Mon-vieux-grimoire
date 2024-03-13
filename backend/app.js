f0daec8e3d4cac6cf197aeb60101d8ed069316dd
Hugo Prevost <43208649+hugoprevost@users.noreply.github.com>
2024-03-13 12:18:08 +0100

    ajout des différentes route et de la base de donnee

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,55 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express()
+const Book = require('./models/Book')
+
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://hugo:hugo@cluster0.yebctzj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !')
-    next()
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
+
+app.post('/api/books', (req, res, next) => {
+  delete req.body._id
+  const book = new Book({
+    ...req.body
   })
-  
-  app.use((req, res, next) => {
-    res.status(201)
-    next()
-  })
-  
-  app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' })
-    next()
-  })
-  
-  app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !')
-  })
+  book.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }))
+})
+
+app.get('/api/books', (req, res, next) => {
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }))
+})
+
+app.get('/api/books/:id', (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }))
+})
+
+app.put('/api/books/:id', (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }))
+})
+
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }))
+})
 
 module.exports = app
---
+++ backend/models/Book.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const bookSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  ratings: [
+    {
+        userId: { type: String, required: true },
+        grade: { type: Number, required: true },
+    }
+  ],
+  averageRating: { type: Number, required: true },
+})
+
+module.exports = mongoose.model('Book', bookSchema)
